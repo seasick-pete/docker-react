a03b90f210b276d58e1fdf0d561394c71d0c74a0
petemap <pete@mapstand.com>
2021-05-10 21:19:06 +0100

    WIP: changed paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye bye, hello
+          Bye bye, hello - done from other branch
         </p>
         <a
           className="App-link"
